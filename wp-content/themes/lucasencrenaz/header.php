92d74725c28c7e5d8590afe1a21957907003fd62
Lucas <lucas.encrenaz@yahoo.fr>
2021-02-22 12:53:32 +0100

    Add title/description/icon

--- wp-content/themes/lucasencrenaz/header.php
+++ wp-content/themes/lucasencrenaz/header.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <meta name="description" content="<?php bloginfo('description'); ?>">
+    <title><?php bloginfo('name'); ?></title>
+    <link rel="shortcut icon" type="image/x-icon" href="<?php get_site_icon_url(); ?>">
     <link href="https://fonts.googleapis.com/css?family=Spartan&display=swap" rel="stylesheet">
     <?php wp_head(); ?>
 </head>
